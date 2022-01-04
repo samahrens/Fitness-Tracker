02ab4aa81842316bc271e3a4ab0701460291ac53
samahrens <89820822+samahrens@users.noreply.github.com>
2022-01-04 00:24:18 -0800

    Completed deleting nutritional data

--- FitnessTracker.java
+++ FitnessTracker.java
@@ -1,3 +1,5 @@
+import com.sun.jdi.request.DuplicateRequestException;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -9,6 +11,7 @@ import java.util.ArrayList;
 public class FitnessTracker extends JPanel {
     private static JFrame frame;
     String dayOfWeek;
+    String dayOfWeek2;
     int calories;
     int protein;
     int carbs;
@@ -20,13 +23,19 @@ public class FitnessTracker extends JPanel {
     private JTextField enterProtein;
     private JTextField enterCarbohydrates;
     private JButton enter1;
-
+    //tab 2:
+    private final String[] choices2 = {"Select Day Of Week", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+    private JComboBox<String> comboBox2 = new JComboBox<>(choices2);
+    private JButton deleteData;
 
     ActionListener actionListener = new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
             if (e.getSource() == enter1) {
                 InputDailyNutrition();
+            }
+            if (e.getSource() == deleteData) {
+                DeleteDailyNutrition();
             }
         }
     };
@@ -38,39 +47,70 @@ public class FitnessTracker extends JPanel {
             if (dayOfWeek.equals("Select Day Of Week")) {
                 throw new Exception();
             }
+
             //throws NumberFormatException
             calories = Integer.parseInt(enterCalories.getText());
             protein = Integer.parseInt(enterProtein.getText());
             carbs = Integer.parseInt(enterCarbohydrates.getText());
+
+            //throws exception about duplicate day of week
+            for (int i = 0; i < dailyInformation.size(); i++) {
+                if (dailyInformation.get(i).toString().contains(dayOfWeek)) {
+                    throw new DuplicateRequestException();
+                }
+            }
             dailyInformation.add(new FitnessTracker(dayOfWeek, calories, protein, carbs));
             System.out.println(dailyInformation.toString());
+
+            JOptionPane.showMessageDialog(null, "Data added successfully!", "Success",
+                    JOptionPane.INFORMATION_MESSAGE);
         } catch (NumberFormatException e) {
             JOptionPane.showMessageDialog(null, "Please enter a number and try again.", "Error",
+                    JOptionPane.ERROR_MESSAGE);
+        } catch (DuplicateRequestException e) {
+            JOptionPane.showMessageDialog(null, "Nutritional information for this day of the week is already full.", "Error",
                     JOptionPane.ERROR_MESSAGE);
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Please select a valid option and try again.", "Error",
                     JOptionPane.ERROR_MESSAGE);
         }
-//        try {
-//            calories = Integer.parseInt(enterCalories.getText());
-//            protein = Integer.parseInt(enterProtein.getText());
-//            carbs = Integer.parseInt(enterCarbohydrates.getText());
-//            dailyInformation.add(new FitnessTracker(dayOfWeek, calories, protein, carbs));
-//            System.out.println(dailyInformation.toString());
-//        } catch (NumberFormatException e) {
-//            JOptionPane.showMessageDialog(null, "Please enter a number and try again.", "Error",
-//                    JOptionPane.ERROR_MESSAGE);
-//        }
+    }
+
+    public void DeleteDailyNutrition() {
+        try {
+            dayOfWeek2 = (String) comboBox2.getSelectedItem();
+            if (dayOfWeek2.equals("Select Day Of Week")) {
+                throw new IllegalArgumentException();
+            }
+
+            if (!dailyInformation.toString().contains(dayOfWeek2)) {
+                throw new Exception();
+            }
+        } catch (IllegalArgumentException e) {
+            JOptionPane.showMessageDialog(null, "Please select a valid option and try again.", "Error",
+                    JOptionPane.ERROR_MESSAGE);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Nutritional data for this day of the week is empty.", "Error",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+
+        for (int i = 0; i < dailyInformation.size(); i++) {
+            if (dailyInformation.get(i).toString().contains(dayOfWeek2)) {
+                dailyInformation.remove(dailyInformation.get(i));
+            }
+        }
+
+        JOptionPane.showMessageDialog(null, "Data removed successfully!", "Success",
+                JOptionPane.INFORMATION_MESSAGE);
     }
 
     public FitnessTracker() {
         super(new GridLayout(1, 1));
         JTabbedPane tabbedPane = new JTabbedPane();
+
+        ///////////
         JComponent InputDailyNutrition = new JPanel();
         InputDailyNutrition.setLayout(null);
-
-        String[] choices = {"Select Day Of Week", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
-//        final JComboBox<String> comboBox = new JComboBox<>(choices);
         comboBox.setBounds(225, 25, 140, 25);
         InputDailyNutrition.add(comboBox);
         enterCalories = new JTextField("Enter Calories (kcal)", 15);
@@ -91,9 +131,16 @@ public class FitnessTracker extends JPanel {
         InputDailyNutrition.add(enter1);
         tabbedPane.addTab("Input Daily Nutrition", InputDailyNutrition);
 
-        JComponent test = new JPanel();
-        tabbedPane.addTab("test tab", test);
-
+        ///////////
+        JComponent DeleteDailyNutrition = new JPanel();
+        DeleteDailyNutrition.setLayout(null);
+        comboBox2.setBounds(225, 25, 140, 25);
+        DeleteDailyNutrition.add(comboBox2);
+        deleteData = new JButton("Delete Data");
+        deleteData.addActionListener(actionListener);
+        deleteData.setBounds(225, 75, 140, 25);
+        DeleteDailyNutrition.add(deleteData);
+        tabbedPane.addTab("Delete Daily Nutrition", DeleteDailyNutrition);
 
 
         add(tabbedPane);
@@ -115,11 +162,9 @@ public class FitnessTracker extends JPanel {
     private static void createAndShowGUI() {
         frame = new JFrame("Fitness Tracker");
         frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-        frame.addWindowListener( new WindowAdapter()
-        {
-            public void windowClosing(WindowEvent e)
-            {
-                JFrame frame = (JFrame)e.getSource();
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                JFrame frame = (JFrame) e.getSource();
 
                 int result = JOptionPane.showConfirmDialog(
                         frame,
